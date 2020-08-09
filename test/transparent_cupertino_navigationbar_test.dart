a87d2214887eb376505cfa8fcfd5ed9e0a1282c2
reforge42 <13710341791@163.com>
2020-08-08 20:03:03 -0400

    first commit

---
+++ test/transparent_cupertino_navigationbar_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:transparent_cupertino_navigationbar/transparent_cupertino_navigationbar.dart';
+
+void main() {
+  test('adds one to input values', () {
+    
+  });
+}
